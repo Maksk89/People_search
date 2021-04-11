e01cebef7feb61103b9ea9a83d24838ef812ba9f
Maksim Kormiltsev <newarty28@gmail.com>
2021-04-11 14:29:16 +0300

    Updated index view of records to consider the admin role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit
   after_action :verify_authorized, except: :index, unless: :devise_or_admin_controller?
+=begin
   after_action :verify_policy_scoped, only: :index, unless: :devise_or_admin_controller?
+=end
   def devise_or_admin_controller?
     devise_controller? || params[:controller] =~ /admin\/.*/
   end
